3c68d1911b037e96136538605bb6deadb3c61822
ItsRiggyman <isaacgamingclowes@gmail.com>
2020-02-22 21:32:43 +0000

    Majorly updated

--- scramble-bot.py
+++ scramble-bot.py
@@ -11,23 +11,29 @@ def main():
     notation = ["L", "L'", "L2", "R", "R'", "R2", "U", "U'", "U2", "D", "D'", "D2", "F",
                 "F'", "F2", "B", "B'", "B2"]
 
-    for i in range(moves):
+    while len(scramble) != moves:
         if not bool(scramble):
             move = random.choice(notation)
             scramble.append(move)
         else:
-            move = random.choice(notation)
-            if move == scramble[-1] or move[0] == scramble[-1][0]:
-                scramble.pop()
+            if scramble[-1][0] == "L" or scramble[-1][0] == "R":
+                selection = [item for item in notation if item[0] not in ("R", "L")]
+                move = random.choice(selection)
+                scramble.append(move)
+            elif scramble[-1][0] == "U" or scramble[-1][0] == "D":
+                selection = [item for item in notation if item[0] not in ("U", "D")]
+                move = random.choice(selection)
                 scramble.append(move)
             else:
+                selection = [item for item in notation if item[0] not in ("F", "B")]
+                move = random.choice(selection)
                 scramble.append(move)
 
     print("Your scramble is: " + ' '.join(scramble))
     answer = input("\nWould you like another scramble (Y/N): ")
 
     if answer.upper() == "Y":
-        os.system("cls")
+        os.system("clear")
         main()
     else:
         print("Goodbye!")
