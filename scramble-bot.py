e14616c264b2bd60973563eb01ee5da2fe45dc8e
Isaac Clowes <53998905+ItsRiggyman@users.noreply.github.com>
2020-10-11 09:08:16 +0100

    Update scramble-bot.py

--- scramble-bot.py
+++ scramble-bot.py
@@ -7,7 +7,7 @@ def main():
     scramble = []
 
     with open("config.json") as f:
-    	moves = int(json.loads(f.read())[0]['moves']) #Reads the preffered move count in the JSON file (default:15)
+        moves = int(json.loads(f.read())[0]['moves']) #Reads the preffered move count in the JSON file (default:15)
 
     notation = ["L", "L'", "L2", "R", "R'", "R2", "U", "U'", "U2", "D", "D'", "D2", "F",
                 "F'", "F2", "B", "B'", "B2"]
@@ -17,16 +17,28 @@ def main():
             move = random.choice(notation)
             scramble.append(move)
         else:
-            if scramble[-1][0] == "L" or scramble[-1][0] == "R":
-                selection = [item for item in notation if item[0] not in ("R", "L")]
+            if scramble[-1][0] == "L":
+                selection = [item for item in notation if item[0] not in "L"]
                 move = random.choice(selection)
                 scramble.append(move)
-            elif scramble[-1][0] == "U" or scramble[-1][0] == "D":
-                selection = [item for item in notation if item[0] not in ("U", "D")]
+            elif scramble[-1][0] == "R":
+                selection = [item for item in notation if item[0] not in "R"]
+                move = random.choice(selection)
+                scramble.append(move)
+            elif scramble[-1][0] == "U":
+                selection = [item for item in notation if item[0] not in "U"]
+                move = random.choice(selection)
+                scramble.append(move)
+            elif scramble[-1][0] == "D":
+                selection = [item for item in notation if item[0] not in "D"]
+                move = random.choice(selection)
+                scramble.append(move)
+            elif scramble[-1][0] == "F":
+                selection = [item for item in notation if item[0] not in "F"]
                 move = random.choice(selection)
                 scramble.append(move)
             else:
-                selection = [item for item in notation if item[0] not in ("F", "B")]
+                selection = [item for item in notation if item[0] not in "B"]
                 move = random.choice(selection)
                 scramble.append(move)
 
